77574d2ef6f8b152ef588ebd7cd961731c7ffbda
FahTw <144051432+FahTw@users.noreply.github.com>
2025-03-11 09:24:37 -0500

    Add files via upload

--- src/main/scala/com/example/JsonFormats.scala
+++ src/main/scala/com/example/JsonFormats.scala
@@ -8,7 +8,7 @@ object JsonFormats {
   import DefaultJsonProtocol._
 
   // ตรวจสอบว่า Game ถูกประกาศเป็น case class แยกจาก object
-  implicit val gameJsonFormat: RootJsonFormat[Game] = jsonFormat8(Game)
+  implicit val gameJsonFormat: RootJsonFormat[Game] = jsonFormat5(Game)
   implicit val gamesJsonFormat: RootJsonFormat[Games] = jsonFormat1(Games)
 
   implicit val actionPerformedJsonFormat: RootJsonFormat[ActionPerformed] = jsonFormat1(ActionPerformed)
